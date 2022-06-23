95ca1ac2fff67f066c1e88388239c01dbff14f0e
Maksym <bilanych.max98009@gmail.com>
2022-06-23 12:18:41 +0300

    remove item in nav

--- src/js/modules/openMenu.js
+++ src/js/modules/openMenu.js
@@ -4,7 +4,18 @@ const openMenu = (triggerBtn, selectorMenu) => {
 
   btn.addEventListener('click', () => {
     btn.classList.toggle('button__menu_active');
-    menu.classList.toggle('header-block__left_active')
+    menu.classList.toggle('header-block__left_active');
+  });
+
+  const itemsMenu = menu.querySelectorAll('.header-nav .header-nav__link');
+
+  itemsMenu.forEach(item => {
+    item.addEventListener('click', () => {
+      if (btn.classList.contains('button__menu_active')) {
+        btn.classList.toggle('button__menu_active');
+        menu.classList.toggle('header-block__left_active');
+      }
+    });
   });
 };
 
